845c8539b493b85e7fc36264e102e4c3039b25f6
Priapril08 <priapril08@gmail.com>
2023-04-01 22:29:54 -0300

    projeto-blackjack

--- nossoJogo.js
+++ nossoJogo.js
@@ -5,8 +5,39 @@
     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
     
     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+    console.log(carta.valor) // imprime o valor da carta (um número). 
+    //Exemplo: 10 (dado que "K" vale 10)
  * 
  * 
  * 
  */
+
+ 
+console.log("Seja benvind_ ao jogo BlackJack!")
+
+let primeiraCartaUser = comprarCarta() 
+let segundaCartaUser = comprarCarta() 
+let pontuacaoUser = primeiraCartaUser.valor + segundaCartaUser.valor
+
+let primeiraCartaComp = comprarCarta() 
+let segundaCartaComp = comprarCarta() 
+let pontuacaoComp = primeiraCartaComp.valor + segundaCartaComp.valor
+
+
+if(confirm("Quer iniciar uma rodada?")) { 
+    console.log("Vamos iniciar uma rodada!")
+    console.log(`Usuário essas são suas cartas: ${primeiraCartaUser.texto} e ${segundaCartaUser.texto} \nTotal:${pontuacaoUser} `)
+    console.log(`Computador essas são suas cartas: ${primeiraCartaComp.texto} e ${segundaCartaComp.texto} \nTotal:${pontuacaoComp} `)
+
+   if(pontuacaoUser < pontuacaoComp) {
+      console.log("Usuário, você perdeu!")
+   } else if (pontuacaoUser > pontuacaoComp ) {   
+      console.log("Usuário, você venceu!") 
+   } else if(pontuacaoUser === pontuacaoComp) {
+      console.log("Usuário e computador, vocês empataram!")
+   }         
+}     
+else { 
+     console.log("O jogo acabou!") 
+ } 
+
